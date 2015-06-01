331ed3e20d5fa018159c13f2e8ab81cedf17a7d4
jwbats <jwbats@gmail.com>
2015-06-01 21:27:59 +0200

    Initial commit.

---
+++ BookCollection/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BookCollection.Models;
+
+namespace BookCollection.Controllers
+{
+	public class HomeController : Controller
+	{
+		private void InitSession()
+		{
+			if (Session["BookCollection"] == null)
+			{
+				Session["BookCollection"] = new List<Book>();
+			}
+		}
+
+
+		private void _AddBook(Book book)
+		{
+			InitSession();
+
+			(Session["BookCollection"] as List<Book>).Add(book);
+		}
+
+
+		public ActionResult Index()
+		{
+			List<Book> books = (Session["BookCollection"] != null)
+				? Session["BookCollection"] as List<Book>
+				: new List<Book>();
+
+			return View(books);
+		}
+
+		
+		[HttpGet]
+		public ActionResult AddBook()
+		{
+			return View();
+		}
+
+		
+		[HttpPost]
+		public ActionResult AddBook(Book book)
+		{
+			_AddBook(book);
+
+			if (ModelState.IsValid)
+				return RedirectToAction("Index");
+			else
+				return View();
+		}
+	}
+}
---
+++ BookCollection/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookCollection.Models
+{
+	public class Book
+	{
+		[Required]
+		public string AuthorName { get; set; }
+		
+		[Required]
+		public string Title { get; set; }
+		
+		public string Publisher { get; set; }
+
+		[DataType(DataType.Date)] // datetime picker only works on chrome
+		public DateTime PublishDate { get; set; }
+	}
+}
