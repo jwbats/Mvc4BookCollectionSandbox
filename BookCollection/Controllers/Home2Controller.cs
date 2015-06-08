e8b13b9f0e594d0f1596b882bf2b047fddf73834
jwbats <jwbats@gmail.com>
2015-06-08 14:39:12 +0200

    Added another controller/view for DVDs.

---
+++ BookCollection/Controllers/Home2Controller.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BookCollection.Models;
+
+namespace BookCollection.Controllers
+{
+	public class Home2Controller : Controller
+	{
+
+		#region ================================================== Private Methods ==================================================
+
+		private void InitSession()
+		{
+			if (Session["DVDCollection"] == null)
+			{
+				Session["DVDCollection"] = new List<DVD>
+				{
+					new DVD()
+					{
+						Title = "Terminator 2",
+						ProducerName = "James Cameron",
+						PublishDate = new DateTime(1990, 3, 1)
+					}
+				};
+			}
+		}
+
+		#endregion ================================================== Private Methods ==================================================
+
+
+
+
+		#region ================================================== Public Methods ==================================================
+
+		public ActionResult Index2()
+		{
+			InitSession();
+
+			List<DVD> dvds = Session["DVDCollection"] as List<DVD>;
+
+			return View(dvds);
+		}
+
+
+
+
+		[HttpGet]
+		public ActionResult AddDVD()
+		{
+			return View();
+		}
+
+
+
+
+		[HttpPost]
+		public ActionResult AddDVD(DVD dvd)
+		{
+			List<DVD> dvds = Session["DVDCollection"] as List<DVD>;
+
+			dvds.Add(dvd);
+
+			if (ModelState.IsValid)
+			{
+				return RedirectToAction("Index2");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		#endregion ================================================== Public Methods ==================================================
+
+	}
+}
---
+++ BookCollection/Models/DVD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookCollection.Models
+{
+	public class DVD
+	{
+		[Required]
+		public string ProducerName { get; set; }
+		
+		[Required]
+		public string Title { get; set; }
+		
+		[DataType(DataType.Date)] // datetime picker only works on chrome
+		public DateTime PublishDate { get; set; }
+	}
+}
